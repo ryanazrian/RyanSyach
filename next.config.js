0dab9083a4c1c2847d35445abe889e2519523c8d
ryanazrian <azrian.ryan@gmail.com>
2024-04-28 15:00:08 +0700

    changes the deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
+  output: 'export',
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
 
@@ -24,6 +25,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
